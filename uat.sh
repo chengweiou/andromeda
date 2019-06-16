6c222170a15b30706a13258070922757bb84b65f
chengweiou <chengwei.ou.dora@gmail.com>
2019-06-15 17:14:23 -0700

    uat path

--- uat.sh
+++ uat.sh
@@ -1,7 +1,7 @@
 ./gradlew bootJar
-cp build/libs/andromeda-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universeandromeda/ser.jar
-cp src/main/resources/application-uat.yml ~/Desktop/docker/universeandromeda/config/
-cp src/main/resources/log4j2.xml ~/Desktop/docker/universeandromeda/config/
-cd ~/Desktop/docker/universeandromeda
+cp build/libs/andromeda-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/andromeda/ser.jar
+cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/andromeda/config/
+cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/andromeda/config/
+cd ~/Desktop/docker/universe/andromeda
 docker stop andromeda
-docker run --rm -it -d --name andromeda -p 60002:8906 --network net -v /Users/chengweiou/Desktop/docker/universeandromeda:/proj/ -w /proj/ openjdk java -jar ser.jar
+docker run --rm -it -d --name andromeda -p 60002:8906 --network net -v /Users/chengweiou/Desktop/docker/universe/andromeda:/proj/ -w /proj/ openjdk java -jar ser.jar
