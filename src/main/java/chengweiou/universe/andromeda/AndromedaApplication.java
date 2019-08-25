8523a90c02314dff03721fc2ada73dc6c5985265
chengweiou <chengwei.ou.dora@gmail.com>
2019-08-24 21:39:22 -0700

    disabled spring security

--- src/main/java/chengweiou/universe/andromeda/AndromedaApplication.java
+++ src/main/java/chengweiou/universe/andromeda/AndromedaApplication.java
@@ -2,9 +2,8 @@ package chengweiou.universe.andromeda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class AndromedaApplication {
 
 	public static void main(String[] args) {
