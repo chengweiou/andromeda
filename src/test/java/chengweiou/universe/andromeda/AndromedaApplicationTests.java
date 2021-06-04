75ef648c1672b7dcf7379a1df404600751d2cb16
chengweiou <chengwei.ou.dora@gmail.com>
2021-06-03 18:27:28 -0700

    springboot 2.5, docker-compose upgrade

--- src/test/java/chengweiou/universe/andromeda/AndromedaApplicationTests.java
+++ src/test/java/chengweiou/universe/andromeda/AndromedaApplicationTests.java
@@ -3,8 +3,10 @@ package chengweiou.universe.andromeda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AndromedaApplicationTests {
 
 	@Test
--- uat.sh
+++ uat.sh
@@ -1,7 +1,13 @@
 ./gradlew bootJar
 cp build/libs/andromeda-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/andromeda/ser.jar
+#cp src/main/resources/application.yml ~/Desktop/docker/universe/andromeda/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/andromeda/config/
 cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/andromeda/config/
-cd ~/Desktop/docker/universe/andromeda
-docker stop andromeda
-docker run --rm -it -d --name andromeda -p 60002:8906 --network net -v ~/Desktop/docker/universe/andromeda:/proj/ -w /proj/ openjdk java -jar ser.jar
+
+docker-compose down
+docker-compose rm -f
+docker-compose up -d
+
+# cd ~/Desktop/docker/universe/andromeda
+# docker stop andromeda
+# docker run --rm -it -d --name andromeda -p 60002:8906 --network net -v ~/Desktop/docker/universe/andromeda:/proj/ -w /proj/ openjdk java -jar ser.jar
