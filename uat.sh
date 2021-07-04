4cfc5476a190cc2b94269934ffa5286f4ea11a58
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-07-03 19:17:02 -0700

    cp jwt pem to uat

--- uat.sh
+++ uat.sh
@@ -3,6 +3,8 @@ cp build/libs/andromeda-0.0.1-SNAPSHOT.jar ~/Desktop/docker/universe/andromeda/s
 #cp src/main/resources/application.yml ~/Desktop/docker/universe/andromeda/config/
 cp src/main/resources/application-uat.yml ~/Desktop/docker/universe/andromeda/config/
 cp src/main/resources/log4j2.xml ~/Desktop/docker/universe/andromeda/config/
+cp src/main/resources/jwt-public.pem ~/Desktop/docker/universe/andromeda/config/
+cp src/main/resources/jwt-private.pem ~/Desktop/docker/universe/andromeda/config/
 cp docker-compose.yml ~/Desktop/docker/universe/andromeda/docker-compose.yml
 cd ~/Desktop/docker/universe/andromeda
 
